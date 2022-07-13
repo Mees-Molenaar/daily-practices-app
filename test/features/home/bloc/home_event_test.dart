8ac850afdecd2082d417421d2c912473dafe389a
Mees Molenaar <108014167+MeesMolenaar@users.noreply.github.com>
2022-07-13 13:35:32 +0200

    Fixed flutter analyze issues

--- test/features/home/bloc/home_event_test.dart
+++ test/features/home/bloc/home_event_test.dart
@@ -1,11 +1,8 @@
-import 'package:daily_practices_api/daily_practices_api.dart';
 import 'package:daily_practices_app/features/home/bloc/home_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('HomeEvent', () {
-    const mockPractice = DailyPractice(id: 1, practice: 'practice');
-
     group('HomeSubscriptionRequested', () {
       test('supports value equality', () {
         expect(
