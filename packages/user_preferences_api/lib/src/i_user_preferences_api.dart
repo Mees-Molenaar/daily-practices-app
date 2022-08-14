762e1711dea598b66aa18ce20960de428fbb33fd
Mees Molenaar <108014167+MeesMolenaar@users.noreply.github.com>
2022-08-14 11:38:10 +0200

    feat: adjust interface to match local user preferences implementation

--- packages/user_preferences_api/lib/src/i_user_preferences_api.dart
+++ packages/user_preferences_api/lib/src/i_user_preferences_api.dart
@@ -3,5 +3,6 @@ import 'package:user_preferences_api/src/models/user_preferences.dart';
 abstract class IUserPreferencesApi {
   const IUserPreferencesApi();
 
-  UserPreferences getUserPreferences();
+  UserPreferences get userPreferences;
+  set lastUpdated(DateTime lastUpdated);
 }
